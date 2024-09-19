d972632b8da52e0385ba3bef5165f08673a28299
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-09-19 10:01:55 -0500

    sliders

--- components/slider/Slider.jsx
+++ components/slider/Slider.jsx
@@ -1,6 +1,14 @@
-import { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { useEffect, useState, memo } from "react";
+import { View, FlatList, Image, StyleSheet, Dimensions } from "react-native";
 import { getSliders } from "../../services/firebaseServices";
+
+const SliderItem = memo(({ item }) => {
+  return (
+    <View style={styles.sliderContainer}>
+      <Image style={styles.image} source={{ uri: item?.imageUrl }} />
+    </View>
+  );
+});
 
 export default function Slider() {
   const [slidersData, setSlidersData] = useState([]);
@@ -21,8 +29,28 @@ export default function Slider() {
   console.log("SLIDERS WITHIN COMPONENT: ", slidersData);
 
   return (
-    <View>
-      <Text>Slider</Text>
+    <View style={styles.container}>
+      <FlatList
+        style={styles.list}
+        data={slidersData}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => <SliderItem item={item} />}
+      ></FlatList>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 15
+  },
+  list: {},
+  sliderContainer: {},
+  image: {
+    width: Dimensions.get("screen").width * 0.9,
+    height: 180,
+    borderRadius: 20,
+    marginRight: 15
+  }
+});
