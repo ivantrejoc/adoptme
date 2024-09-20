8d9941f605ddf1826cb5151fa8a24b87170149ec
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-09-20 15:51:29 -0500

    pet details view

--- app/pet-details/index.jsx
+++ app/pet-details/index.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import { useLocalSearchParams, useNavigation } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, ScrollView, View } from "react-native";
 import PetInfo from "../../components/petInfo/PetInfo";
 import PetSubInfo from "../../components/petInfo/PetSubInfo";
+import AboutPet from "../../components/petInfo/AboutPet";
 
 export default function PetDetails() {
   const navigation = useNavigation();
@@ -18,8 +19,11 @@ export default function PetDetails() {
 
   return (
     <View style={styles.container}>
-      <PetInfo image={image} name={name} age={age} breed={breed} />
-      <PetSubInfo age={age} breed={breed} />
+      <ScrollView style={styles.scrollView}>
+        <PetInfo image={image} name={name} age={age} breed={breed} />
+        <PetSubInfo age={age} breed={breed} />
+        <AboutPet name={name} />
+      </ScrollView>
     </View>
   );
 }
---
+++ components/petInfo/AboutPet.jsx
@@ -0,0 +1,61 @@
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import colors from "../../constants/colors";
+import { useState } from "react";
+
+export default function AboutPet({ name }) {
+  const [readMore, SetReadMore] = useState(true);
+
+  const handleReadMorePress = () => {
+    SetReadMore(false);
+  };
+
+  const handleReadLessPress = () => {
+    SetReadMore(true);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>About {name}</Text>
+      <Text numberOfLines={readMore ? 3 : 20} style={styles.paragraph}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Text>
+
+      {readMore && (
+        <Pressable onPress={handleReadMorePress}>
+          <Text style={styles.textBtn}>Read More</Text>
+        </Pressable>
+      )}
+      {!readMore && (
+        <Pressable onPress={handleReadLessPress}>
+          <Text style={styles.textBtn}>Read Less</Text>
+        </Pressable>
+      )}
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    padding: 20
+  },
+  title: {
+    fontFamily: "lato",
+    fontSize: 20,
+    fontWeight: "600"
+  },
+  paragraph: {
+    fontFamily: "lato",
+    fontSize: 14
+  },
+  textBtn: {
+    fontFamily: "lato",
+    fontSize: 14,
+    fontWeight: "700",
+    color: colors.SECONDARY
+  }
+});
